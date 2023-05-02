3d318443c9aa7807e891306ce91644ddc1981225
AmanWorku <amexworku2014@gmail.com>
2023-05-02 18:49:39 +0300

    Configure route and create recipe index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'user/index'
   devise_for :users
+
+  resources :recipes
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "user#index"
   # Defines the root path route ("/")

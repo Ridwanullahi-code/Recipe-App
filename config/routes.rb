693a35ff2d7e40a3e93e13d7be870a8b243a6073
ridwanullahi <ridwanullahiajayi01@gmail.com.com>
2023-05-02 05:25:44 +0100

    complete devise setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'user/index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "user#index"

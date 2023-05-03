18f72232ca1fdb163b57fcf987e4c4324133724c
AmanWorku <amexworku2014@gmail.com>
2023-05-03 10:34:26 +0300

    Add many to many relationship to the inventory

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,4 +1,5 @@
 class Inventory < ApplicationRecord
   belongs_to :user
   has_many :inventory_foods, foreign_key: 'inventory_id', dependent: :destroy
+  has_many :foods, through: :inventory_foods, dependent: :destroy
 end

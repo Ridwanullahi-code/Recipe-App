185e190f0d86782901e69e6cf332a5b84fc02992
AmanWorku <amexworku2014@gmail.com>
2023-05-04 08:09:20 +0300

    Add necessary files

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,5 +1,4 @@
 class Inventory < ApplicationRecord
   belongs_to :user
   has_many :inventory_foods, foreign_key: 'inventory_id', dependent: :destroy
-  has_many :foods, through: :inventory_foods, dependent: :destroy
 end
